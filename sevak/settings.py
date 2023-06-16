65741b2ec18e35fe2b3210c67444b60df739c707
Royal Simpson Pinto <royalpinto007@gmail.com>
2023-06-16 09:59:09 +0530

    fix: Resolve warning for missing 'static' directory in STATICFILES_DIRS

--- sevak/settings.py
+++ sevak/settings.py
@@ -138,4 +138,4 @@ import os
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "hck/static")]
